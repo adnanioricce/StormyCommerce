07b57f440752b88fddec5aa129de3d8eb640bd62
adnanioricce <adnanioricce@outlook.com>
2019-08-11 23:28:24 -0300

    adding docker and bogus

---
+++ src/StormyCommerce.Infraestructure/Extensions/StormyDbContextExtensions.cs
@@ -0,0 +1,12 @@
+﻿using StormyCommerce.Infraestructure.Data;
+
+namespace StormyCommerce.Infraestructure.Extensions
+{
+    public static class StormyDbContextExtensions
+    {
+        public static void SeedDatabase(this StormyDbContext stormyDbContext)
+        {
+            
+        }
+    }
+}
