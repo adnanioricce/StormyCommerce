961a04ebd03363194ac0365adbcc4b142cc525bc
stormytcc <adnangonzaga@gmail.com>
2019-08-28 17:56:00 -0300

    rewriting test and logic

--- test/Modules/Modules.Tests/Customers/AuthenticationControllerTest.cs
+++ test/Modules/Modules.Tests/Customers/AuthenticationControllerTest.cs
@@ -13,33 +13,52 @@ using StormyCommerce.Infraestructure.Helpers;
 using StormyCommerce.Module.Customer.Area.ViewModels;
 using StormyCommerce.Module.Customer.Controllers;
 using StormyCommerce.Module.Customer.Services;
+using SimplCommerce.Module.EmailSenderSendgrid;
 using TestHelperLibrary.Mocks;
 using Xunit;
+using Microsoft.Extensions.Configuration;
 
 namespace Modules.Test.Customers
 {
     public class AuthenticationControllerTest
-    {
-        private UserIdentityService GetUserIdentityService()
+    {        
+        private readonly ApplicationUser appUserSample;
+        private readonly AuthenticationController _controller;
+        private UserIdentityService GetUserIdentityService(FakeUserManager fakeUserManager)
         {
-            return new UserIdentityService(new FakeSignInManager(), new FakeUserManager(), null);
+            return new UserIdentityService(new FakeSignInManager(), fakeUserManager, null);
         }
-        public AuthenticationController CreateController()
+        public AuthenticationControllerTest()
         {            
-            //fakeIdentityService
             var fakeTokenService = new Mock<ITokenService>();
             fakeTokenService
                 .Setup(f => f.GenerateAccessToken(It.IsAny<IEnumerable<Claim>>()))
                 .Returns(It.IsAny<string>());
-            //TODO:Create the EmailSender mock
-            var controller = new AuthenticationController(GetUserIdentityService(), fakeTokenService.Object, null);
-            return controller;
+            var fakeUserManager = new FakeUserManager();            
+            appUserSample = fakeUserManager.Users.FirstOrDefault();             
+            _controller = new AuthenticationController(
+             GetUserIdentityService(fakeUserManager),
+             fakeTokenService.Object,
+             GetFakeEmailSender());
+        }        
+        private EmailSender GetFakeEmailSender()
+        {
+            var fakeConfigSection = new Mock<IConfigurationSection>();
+            fakeConfigSection.SetupGet(f => f[It.IsAny<string>()]).Returns("fake_result");
+            // fakeConfigSection.SetupGet(f => f[It.Is<string>(s => s == "SendGrid:Email:ApiKey")])
+            // .Returns("fakeKey");
+            // fakeConfigSection.SetupGet(f => f[It.Is<string>()])
+            var fakeConfiguration = new Mock<IConfiguration>();
+            fakeConfiguration
+                .Setup(f => f.GetValue(It.IsAny<string>(),It.IsAny<string>()))
+                .Returns(Guid.NewGuid().ToString("N"));                                   
+            var fakeEmailSender = new Mock<EmailSender>(fakeConfiguration.Object);                                                
+            return fakeEmailSender.Object;
         }
         [Fact]
         public async Task LoginAsync_VmWithValidEmailAndPassword_ShouldReturnJWT()
         {
-            //Arrange
-            var controller = CreateController();
+            //Arrange            
             var loginVm = new SignInVm
             {
                 Username = "someUser",
@@ -47,7 +66,7 @@ namespace Modules.Test.Customers
                 Password = "!Tr27gh43"
             };
             //Act
-            var result = Assert.IsType<OkObjectResult>(await controller.LoginAsync(loginVm));
+            var result = Assert.IsType<OkObjectResult>(await _controller.LoginAsync(loginVm));
             //Assert
             //How do I test it?
             Assert.Equal(200,result.StatusCode);            
@@ -55,16 +74,15 @@ namespace Modules.Test.Customers
         [Fact]
         public async Task RegisterAsync_ReceivedValidVm_ShouldReturnSuccessMessage()
         {
-            //Arrange
-            var controller = CreateController();
-            var registerVm = new SignUpVm
+            //Arrange            
+            var signUpVm = new SignUpVm
             {
                 //TODO:research -> Why we need to Confirm password?
                 Email = "myemail@example.com",
                 Password = "!Tr27gh43"
             };
             //Act 
-            var result = (await controller.RegisterAsync(registerVm)) as OkObjectResult;
+            var result = Assert.IsAssignableFrom<OkObjectResult>(await _controller.RegisterAsync(signUpVm));
             //Assert 
             Assert.NotNull(result);
         }
