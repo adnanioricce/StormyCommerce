8b5ba9478c9af883c5d8da86373d244a3ac98075
danilexx <42390775+danilexx@users.noreply.github.com>
2019-09-01 19:00:18 -0300

    chore: added a toPrice util function to make number to price string conversion standart

--- Web/components/PriceDisplay.jsx
+++ Web/components/PriceDisplay.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import * as React from 'react';
+import toPrice from '../util/toPrice';
 
 export default ({ price, oldPrice }) => {
   const priceParcelado = `R$ ${(price / 3).toFixed(2)}`;
   return (
     <div className="price-display-container">
-      <h1>{`${oldPrice ? `De R$ ${oldPrice.toFixed(2)}` : ''} Por R$ ${price.toFixed(2)}`}</h1>
+      <h1>{`${oldPrice ? `De ${toPrice(oldPrice)}` : ''} Por ${toPrice(
+        price
+      )}`}
+      </h1>
       <span>
         ou <b>6x</b> de <b>{priceParcelado}</b> sem juros
       </span>
